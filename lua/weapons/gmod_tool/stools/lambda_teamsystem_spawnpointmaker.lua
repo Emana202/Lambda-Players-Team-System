0d009c1745d36bc6a75ed5144fe49841fa87ea5c
ErmaaS-41 <emana202@yandex.ru>
2023-02-19 21:28:05 +0600

    Added spaces to 'Spawnpoint' words

--- lua/weapons/gmod_tool/stools/lambda_teamsystem_spawnpointmaker.lua
+++ lua/weapons/gmod_tool/stools/lambda_teamsystem_spawnpointmaker.lua
@@ -7,11 +7,11 @@ if ( CLIENT ) then
 		{ name = "right" }
 	}
 
-	language.Add("tool.lambda_teamsystem_spawnpointmaker", "Lambda Spawnpoint Maker")
-	language.Add("tool.lambda_teamsystem_spawnpointmaker.name", "Lambda Spawnpoint Maker")
-	language.Add("tool.lambda_teamsystem_spawnpointmaker.desc", "Creates a team spawnpoint that Lambda teams will use to spawn in")
-	language.Add("tool.lambda_teamsystem_spawnpointmaker.left", "Fire onto a surface to create spawnpoint or fire near a existing spawnpoint to change its team")
-	language.Add("tool.lambda_teamsystem_spawnpointmaker.right", "Fire near a existing spawnpoint to remove it")
+	language.Add("tool.lambda_teamsystem_spawnpointmaker", "Lambda Team Spawn Point Maker")
+	language.Add("tool.lambda_teamsystem_spawnpointmaker.name", "Lambda Team Spawn Point Maker")
+	language.Add("tool.lambda_teamsystem_spawnpointmaker.desc", "Creates a team spawn point that Lambda Team it belongs to will use to spawn and respawn")
+	language.Add("tool.lambda_teamsystem_spawnpointmaker.left", "Fire onto an empty surface to create spawnpoint or fire near an existing spawn point to change its team to the currenly selected one")
+	language.Add("tool.lambda_teamsystem_spawnpointmaker.right", "Fire near an existing spawn point to remove it")
 
 end
 
@@ -50,10 +50,10 @@ function TOOL:LeftClick( tr )
 			spawnPoint:Spawn()
 
 			local addname = ( teamName != "" and teamName or spawnPoint:GetCreationID() )
-			undo.Create("Lambda Spawnpoint " .. addname )
+			undo.Create("Lambda Team Spawn Point " .. addname )
 				undo.SetPlayer( self:GetOwner() )
 				undo.AddEntity( spawnPoint )
-			undo.Finish("Lambda Spawnpoint " .. addname )
+			undo.Finish("Lambda Team Spawn Point " .. addname )
 
 			self:GetOwner():AddCleanup( "sents", spawnPoint )
     	end
@@ -77,7 +77,7 @@ end
 function TOOL.BuildCPanel( panel )
     local combo = panel:ComboBox( "Spawn Team", "lambda_teamsystem_spawnpointmaker_spawnteam" )
     for k, v in pairs( LambdaTeams.TeamOptions ) do if k != "None" then combo:AddChoice( k, v ) end end
-	panel:ControlHelp( "The team that this spawnpoint will belong to." )
+	panel:ControlHelp( "The team that this spawn point will belong to." )
 
 	local refresh = vgui.Create( "DButton" )
 	panel:AddItem( refresh )
